4d854df968dcb4f52f990ff723e7d9b29c2a7eec
casual-reducer-f401 <lozans89@gmail.com>
2021-03-21 05:21:09 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,7 @@ board = [" ", " ", " "]
 user_input = ["1, 2, 3, 4, 5, 6, 7, 8, 9"]
 
 def input_to_index(user_input)
-  puts ("user_input".to_i) - 1
+puts "user_input".to_i
 end
 
 converted_input = input_to_index(user_input)
